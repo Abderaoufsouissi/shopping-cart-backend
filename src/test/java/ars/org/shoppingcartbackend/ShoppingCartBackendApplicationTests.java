397c18ac20ce0f11261708d2b7ef6f8bb8c6c12c
Abderaoufsouissi <Abderaouf.souissi@outlook.com>
2025-01-01 11:38:01 +0100

    first commit added web, Jpa , lombok , mysql driver and validation dependencies

---
+++ src/test/java/ars/org/shoppingcartbackend/ShoppingCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ars.org.shoppingcartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

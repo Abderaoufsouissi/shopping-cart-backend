57b4e79a66e7fbbeed6346e3ac04ce0e5c99d2dd
Abderaoufsouissi <Abderaouf.souissi@outlook.com>
2025-01-01 12:29:40 +0100

    added the necessary config to application.propreties file

--- src/main/java/ars/org/shoppingcartbackend/model/Product.java
+++ src/main/java/ars/org/shoppingcartbackend/model/Product.java
@@ -1,4 +1,30 @@
 package ars.org.shoppingcartbackend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy ="product", cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Image> images;
 }

c3b66b12a1aa13347abe5fa6c2ebbcbfb8a4e3dc
ars <abderaouf.souissi@outlook.com>
2025-01-22 15:46:25 +0100

    implemented getImageById method

--- src/main/java/ars/org/shoppingcartbackend/service/image/ImageServiceImpl.java
+++ src/main/java/ars/org/shoppingcartbackend/service/image/ImageServiceImpl.java
@@ -1,15 +1,26 @@
 package ars.org.shoppingcartbackend.service.image;
 
+import ars.org.shoppingcartbackend.exceptions.RessourceNotFoundException;
 import ars.org.shoppingcartbackend.model.Image;
+import ars.org.shoppingcartbackend.repository.ImageRepository;
+import ars.org.shoppingcartbackend.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
+@RequiredArgsConstructor
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+    private final ProductRepository productRepository;
+
     @Override
     public Image getImageById(Long id) {
-        return null;
+        return imageRepository.findById(id).orElseThrow(
+                ()-> new RessourceNotFoundException("No image found with id : "+id));
+
+
     }
 
     @Override

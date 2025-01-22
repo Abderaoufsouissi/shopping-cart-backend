a2f4407dccedc95db701a76a82532582f4b03b21
ars <abderaouf.souissi@outlook.com>
2025-01-22 15:40:30 +0100

    added ImageRepository interface

---
+++ src/main/java/ars/org/shoppingcartbackend/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package ars.org.shoppingcartbackend.repository;
+
+import ars.org.shoppingcartbackend.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+
+}
--- src/main/java/ars/org/shoppingcartbackend/service/image/ImageServiceImpl.java
+++ src/main/java/ars/org/shoppingcartbackend/service/image/ImageServiceImpl.java
@@ -1,4 +1,29 @@
 package ars.org.shoppingcartbackend.service.image;
 
-public class ImageServiceImpl {
+import ars.org.shoppingcartbackend.model.Image;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public Image getImageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+
+    }
+
+    @Override
+    public Image saveImage(MultipartFile file, Long productId) {
+        return null;
+    }
+
+    @Override
+    public Void updateImage(MultipartFile file, Long productId) {
+        return null;
+    }
 }

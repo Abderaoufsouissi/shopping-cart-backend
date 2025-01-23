c6413cb0fd6323aa2d9fdacf69f48e0215abe42b
ars <abderaouf.souissi@outlook.com>
2025-01-23 18:01:59 +0100

    added the saveImages request in ImageController

--- src/main/java/ars/org/shoppingcartbackend/controller/ImageController.java
+++ src/main/java/ars/org/shoppingcartbackend/controller/ImageController.java
@@ -1,7 +1,40 @@
 package ars.org.shoppingcartbackend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
+import ars.org.shoppingcartbackend.dto.ImageDto;
+import ars.org.shoppingcartbackend.model.Product;
+import ars.org.shoppingcartbackend.response.ApiResponse;
+import ars.org.shoppingcartbackend.service.image.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("${api.prefix/images")
 public class ImageController {
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files ,@RequestParam Long productId){
+        try {
+            List<ImageDto> imageDtos = imageService.saveImages(files, productId);
+            return ResponseEntity.ok(new ApiResponse("Upload successful",imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Upload failed",e.getMessage()));
+        }
+    }
+
+
+
 }
+
